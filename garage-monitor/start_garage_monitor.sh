a497ba075e222fa2eb63f6ed3eb1839e40152d45
riverwest37 <63129065+riverwest37@users.noreply.github.com>
2026-04-27 11:08:25 +0900

    Update start_garage_monitor.sh

--- garage-monitor/start_garage_monitor.sh
+++ garage-monitor/start_garage_monitor.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# 車庫モニター全画面起動スクリプト
+# 必要に応じて DISPLAY や XAUTHORITY を環境に合わせて変更してください
+
+sleep 10
+
+chromium \
+  --kiosk \
+  --noerrdialogs \
+  --disable-infobars \
+  --disable-session-crashed-bubble \
+  --user-data-dir=/tmp/chromium-garage-monitor \
+  file:///home/pi/garage_monitor/garage_viewer.html
